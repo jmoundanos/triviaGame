8880863fdba653093ebf3ee83385d9426955d54b
Jennifer Moundanos <jmoundanos@gmail.com>
2020-01-08 22:40:09 -0600

    added trivia questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+var gameQuestions = [
+    {
+        question: "Which country owns Antarctica?",
+        answers: [
+            "United States",
+            "Mexico",
+            "China",
+            "None"
+    ],
+        correctAnswer: "None"
+    },
+    {
+        question: "What is the third largest continent?",
+        answers: [
+            "North America",
+            "Europe",
+            "Asia",
+            "Africa"
+        ],
+        correctAnswer: "North America"
+    },
+    {
+        question: "What is the longest river in India?",
+        answers: [
+            "Brahmaputra",
+            "Yamuna",
+            "Ganges",
+            "Sahibi"
+        ],
+        correctAnswer: "Ganges"
+    },
+    {
+        question: "What is the longest continental mountain range?",
+        answers: [
+            "Himilayas",
+            "Andes",
+            "Alps",
+            "Rockies"
+        ],
+        correctAnswer: "Andes"
+    },
+    {
+        question: "What is the largest non-polar desert?",
+        answers: [
+            "Mojave",
+            "Gobi",
+            "Sahara",
+            "Arabian"
+        ],
+        correctAnswer: "Sahara"
+    },
+    {
+        question: "Which city is located in two continents?",
+        answers: [
+            "Instanbul",
+            "Paris",
+            "Tokyo",
+            "New York City"
+        ],
+        correctAnswer: "Istanbul"
+    },
+    {
+        question: "Which African nation has the most pyramids?",
+        answers: [
+            "Egypt",
+            "Morocco",
+            "Algeria",
+            "Sudan"
+        ],
+        correctAnswer: "Sudan"
+    },
+    {
+        question: "What country has the most natural lakes?",
+        answers: [
+            "Canada",
+            "Brazil",
+            "England",
+            "India"
+        ],
+        correctAnswer: "Canada"
+    },
+    {
+        question: "Which city is the most populous?",
+        answers: [
+            "Shanghai",
+            "Mumbai",
+            "Tokyo",
+            "Karachi"
+        ],
+        correctAnswer: "Tokyo"
+    },
+    {
+        question: "Where is the world's tallest mountain?",
+        answers: [
+            "Hawaii",
+            "Japan",
+            "Switzerland",
+            "Nepal"
+        ],
+        correctAnswer: "Nepal"
+    }
+]
